73286c6938f9b8a13f570af58948faa9b9d3701e
Marc SCHNEIDER <marc@mirelsol.org>
2014-03-05 18:39:38 +0100

    Towards to v2.0

--- idioma/admin.py
+++ idioma/admin.py
@@ -2,14 +2,22 @@
 #-*- encoding:utf-8 *-*
 
 from django.contrib import admin
-from idioma.models import ExpressionIta, ExpressionGer, ExpressionEng
+from idioma.models import ExpressionIta, ExpressionGer, ExpressionEng, ExpressionGen, Language, Topic
 
 
 class ExpressionAdmin(admin.ModelAdmin):
     list_display = ('french_expression', 'foreign_expression', 'tatoeba_id', 'added_on', 'updated_on')
     search_fields = ['french_expression', 'foreign_expression']
 
+class ExpressionGenAdmin(admin.ModelAdmin):
+    list_display = ('from_expr', 'to_expr', 'tatoeba_id', 'added_on', 'updated_on')
+    list_filter = ('from_language', 'to_language')
+    search_fields = ['from_expr', 'to_expr']
 
 admin.site.register(ExpressionIta, ExpressionAdmin)
 admin.site.register(ExpressionGer, ExpressionAdmin)
 admin.site.register(ExpressionEng, ExpressionAdmin)
+
+admin.site.register(ExpressionGen, ExpressionGenAdmin)
+admin.site.register(Language)
+admin.site.register(Topic)
--- idioma/models.py
+++ idioma/models.py
@@ -3,6 +3,46 @@
 
 from django.db import models
 
+
+class Language(models.Model):
+    code = models.CharField(max_length=2, unique=True)
+    label = models.CharField(max_length=15, unique=True)
+
+    def __unicode__(self):
+        return "%s" % (self.label)
+
+
+class Topic(models.Model):
+    label = models.CharField(max_length=30, unique=True)
+
+    def __unicode__(self):
+        return "%s" % (self.label)
+
+
+class ExpressionGen(models.Model):
+    added_on = models.DateField() #(auto_now_add=True)
+    updated_on = models.DateField() #(auto_now=True)
+    
+    topic = models.ForeignKey(Topic, null=True, blank=True)
+    
+    from_language = models.ForeignKey(Language, related_name='from_language')
+    from_expr = models.CharField(max_length=255, help_text="Use | to separate multiple expressions")
+    from_comment = models.TextField(max_length=255, null=True, blank=True)
+
+    to_language = models.ForeignKey(Language, related_name='to_language')
+    to_expr = models.CharField(max_length=255, help_text="Use | to separate multiple expressions")
+    to_comment = models.TextField(max_length=255, null=True, blank=True)
+    
+    tatoeba_id = models.IntegerField(null=True, blank=True)
+
+    def __unicode__(self):
+        return "%s [%s]" % (self.from_expr, self.to_expr)
+
+    class Meta:
+        verbose_name = 'Expression'
+        unique_together = ("from_expr", "to_expr")
+
+
 class Expression(models.Model):
     added_on = models.DateField(auto_now_add=True)
     updated_on = models.DateField(auto_now=True)
